4a3a162f1746113656c57acc898f89c7d9ee5a24
dududoxx <eduardonascimentosouza9134@gmail.com>
2024-03-20 20:59:19 -0300

    Aula dia 20

---
+++ 1.py
@@ -0,0 +1,27 @@
+#     print('C é o maior valor!')
+
+# salario = float(input('Qual é o seu salario?'))
+# if salario > 1250:
+#     superior = salario * 1.10
+#     print(f"O salário com aumento é R${superior:.2f}")
+# else:
+#     inferior = salario * 1.15
+#     print(f"o salário com aumento é R${inferior}")
+
+# n1 = float(input("n1 ="))
+# n2 = float(input("n2 ="))
+# n3 = float(input("n3 ="))
+#
+# maior = n1
+# if n2 >= n1 and n2 >= n3:
+#     maior = n2
+# if n3 >= n1 and n3 >= n2:
+#     maior = n3
+#
+# menor = n1
+# if n2 <= n1 and n2 <= n3:
+#     menor = n2
+# if n3 <= n1 and n3 <= n2:
+#     menor = n3
+# print(f"O Maior número é {maior}")
+# print(f"O Menor número é {menor}")
