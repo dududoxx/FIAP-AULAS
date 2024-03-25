a3aa4778ae13de3989a12378b4c264d7d7645d0f
dududoxx <164263883+dududoxx@users.noreply.github.com>
2024-03-25 15:22:20 -0300

    Add files via upload

---
+++ numero = int(input()).py
@@ -0,0 +1,27 @@
+# numero = int(input())
+# horas = int(input())
+# valor = float(input())
+# SALARIO = horas * valor
+# print('numero =', numero)
+# print(f'SALARY = U$ {SALARIO:.2f}')
+
+# nome = input("Qual é o seu nome?")
+# salario = float(input("Qual o seu salário?"))
+# vendas = float(input("Quanto você vendeu?"))
+# T = salario + vendas * 1.15
+# print(f"TOTAL ={T:.2f}") 
+
+# dado = int(input()) , int(input()), float(input())
+# dado_dois = int(input()) , int(input()), float(input())
+# valor = dado + dado_dois
+# print(f"VALOR A PAGAR: R$ {valor:.2f}")
+
+# codigo_peca1, num_peca1, valor_unitario1 = map(float, input().split())
+# codigo_peca2, num_peca2, valor_unitario2 = map(float, input().split())
+# total_a_pagar = (num_peca1 * valor_unitario1) + (num_peca2 * valor_unitario2)
+# print(f"VALOR A PAGAR: R$ {total_a_pagar:.2f}")
+
+R = float(input())
+pi = 3.14159
+volume = (4/3.0) * pi *R**3
+print(f'VOLUME = {volume:.3f}')
