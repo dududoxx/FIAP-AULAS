45724beb0fb49f8413014812df54a8147a74449a
labsfiap <eduardonascimentosouza9134@gmail.com>
2024-08-05 22:39:59 -0300

    sadsa

---
+++ repeticaofor.py
@@ -0,0 +1,60 @@
+# alunos = ["Gustavo" , "Nicolas" , "Lucca" , "Victor"]
+# for i in alunos:
+#     print(i)
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# alunos = ["Gustavo" , "Nicolas" , "Lucca" , "Victor"]
+# nome = input("Entre com o nome do estudante para pesquisar: ").capitalize()
+# for i in alunos:
+#     if i == nome:
+#         print("Aluno encontrado: " + i)
+#         break
+# else:
+#     print("Aluno não encontrado")
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# for i in range(10):
+#     print(i)
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# for v in range(1,11):
+#     print(v)
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# num = 0
+# for i in range(0 , 21 , 2):
+#     print(f"2 x {num} = {i}")
+#     num += 1
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# L = [1, 7, 2, 4]
+# maximo = L[0]
+# for i in L:
+#     if i > maximo:
+#         maximo = i
+# print(maximo)
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# idades = [18, 45, 7, 12]
+# maior = idades[0]
+# for i in idades:
+#     if i > maior:
+#         maior = i
+# print(maior)
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+# idades = [19, 45, 7, 12]
+# print(min(idades))
+# print(max(idades))
+
+#########################AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA####################
+
+T = [-10, -8, 0, 1, 2, 5, -2, -4]
+comparador = T[0]
